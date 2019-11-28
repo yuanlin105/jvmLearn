690dcf7e12249d6a97cea6937cb9dc2b3dd920c9
yuanlin <jatly22@126.com>
2019-11-28 20:39:24 +0800

    初次创集

---
+++ src/com/byteCode/Mytest1.java
@@ -0,0 +1,16 @@
+package com.byteCode;
+
+public class Mytest1 {
+    
+    private int  i  =1;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+    
+
+}
